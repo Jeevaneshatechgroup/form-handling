84e4d96c62370d1351a1e7cfcb956f34b39a5d06
prathikdz <prathik@eshatechgroup.com>
2025-02-24 02:36:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UserForm from "./components/UserForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Handling Forms & Sending Data to an API</h1>
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
